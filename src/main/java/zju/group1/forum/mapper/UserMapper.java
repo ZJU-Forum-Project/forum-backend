c01634332bcce4e28560f6430dbc9f3ae33ed386
iwzy7071 <iwzy7071@gmail.com>
2020-04-09 13:07:56 +0800

    Version1.4 1.Finish Email function 2.Finish Register function

--- src/main/java/zju/group1/forum/mapper/UserMapper.java
+++ src/main/java/zju/group1/forum/mapper/UserMapper.java
@@ -2,12 +2,15 @@ package zju.group1.forum.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import zju.group1.forum.dto.User;
 
 @Mapper
 public interface UserMapper {
 
-    @Insert("insert into user (email,name,account,password) values (#{email},#{name},#{account},#{password}")
+    @Insert("insert into user (email,name,password) values (#{email},#{name},#{password})")
     void createUser(User user);
 
+    @Select("select count(*) from user where name=#{name}")
+    int isUserExist(User user);
 }
---
+++ src/main/java/zju/group1/forum/service/MailService.java
@@ -0,0 +1,34 @@
+package zju.group1.forum.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+@Service
+public class MailService {
+
+
+    @Value("${spring.email.password}")
+    private String password;
+    @Value("${spring.email.username}")
+    private String username;
+    @Value("${spring.email.host}")
+    private String host;
+
+    public void sendToken(String toEmail, String token) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(username);
+        simpleMailMessage.setTo(toEmail);
+        simpleMailMessage.setSubject("Forum注册验证");
+        simpleMailMessage.setText("您的验证码是:" + token + "\n验证码5min内有效");
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.send(simpleMailMessage);
+
+    }
+}
