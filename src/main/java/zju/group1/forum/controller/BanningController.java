0fb7f47d5f80527422094d5952ac4bd5a6ea9333
passionwp <passionwp@foxmail.com>
2020-05-30 15:38:47 +0800

    实现禁言相关接口
    1.实现禁言相关接口，redis数据存储在db1中
    2.指定token数据存储在db0中

--- src/main/java/zju/group1/forum/controller/BanningController.java
+++ src/main/java/zju/group1/forum/controller/BanningController.java
@@ -1,20 +1,29 @@
 package zju.group1.forum.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import zju.group1.forum.dto.Message;
+import zju.group1.forum.interceptor.AuthToken;
+import zju.group1.forum.provider.RedisProvider;
 
 
-//先把接口列出来，后续再实现
 
 
 @Api(tags = "禁言相关操作")
 @RestController
 public class BanningController {
+
+    @Autowired
+    private RedisProvider redisProvider;
+
     @ApiOperation("执行禁言")
+    @PostMapping(value = "/banUser")
+    @AuthToken
     public Message banUser(@RequestParam("Authorization") String token,
                            @RequestParam("author") String author,
                            @RequestParam("banningDays") int banningDays
@@ -22,24 +31,76 @@ public class BanningController {
     {
         Message message = new Message();
 
+        /*case1：未登录*/
+        if (token == null) {
+            message.setState(false);
+            message.setMessage("请重新登录");
+            message.setAuthorizeToken(token);
+            return message;
+        }
+
+
+        /*case2:若用户已被禁言*/
+        if(redisProvider.checkIfBanned(author)){
+            message.setState(false);
+            message.setMessage("该用户已被禁言");
+            message.setAuthorizeToken(token);
+            return message;
+        }
+
+        /*case3:正常禁言流程*/
+        redisProvider.setBannedUser(author, banningDays);
+
+        message.setState(true);
+        message.setMessage("禁言成功");
+        message.setAuthorizeToken(token);
 
         return message;
     }
 
 
     @ApiOperation("取消禁言")
+    @PostMapping(value = "/freeUser")
+    @AuthToken
     public Message freeUser(@RequestParam("Authorization") String token,
                            @RequestParam("author") String author
     )
     {
         Message message = new Message();
 
+        /*case1：未登录*/
+        if (token == null) {
+
+
+            message.setState(false);
+            message.setMessage("请重新登录");
+            message.setAuthorizeToken(token);
+            return message;
+        }
+
+
+        /*case2:若用户未被禁言*/
+        if(redisProvider.checkIfBanned(author) == false){
+            message.setState(false);
+            message.setMessage("该用户未被禁言");
+            message.setAuthorizeToken(token);
+            return message;
+        }
+
+        /*case3:正常解禁流程*/
+        redisProvider.freeBannedUser(author);
+
+        message.setState(true);
+        message.setMessage("解禁成功");
+        message.setAuthorizeToken(token);
 
         return message;
     }
 
 
     @ApiOperation("查询是否为禁言状态")
+    @PostMapping(value = "/checkIfBanned")
+    @AuthToken
     public Message checkIfBanned(@RequestParam("Authorization") String token,
                                 @RequestParam("author") String author
     )
@@ -47,9 +108,20 @@ public class BanningController {
         Message message = new Message();
 
 
+        if(redisProvider.checkIfBanned(author)){
+
+            message.setState(true);
+            message.setMessage("用户为禁言状态");
+            message.setAuthorizeToken(token);
+        }
+
+        else{
+            message.setState(false);
+            message.setMessage("用户不在禁言状态");
+            message.setAuthorizeToken(token);
+        }
+
+
         return message;
     }
-
-
-
 }
--- src/main/java/zju/group1/forum/provider/RedisProvider.java
+++ src/main/java/zju/group1/forum/provider/RedisProvider.java
@@ -24,9 +24,20 @@ public class RedisProvider {
         return new JedisPool(jedisPoolConfig, redisHost, Integer.parseInt(redisPort));
     }
 
+    /*本地测试用*/
+    private JedisPool getLocalRedisPool() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(10);
+        return new JedisPool(jedisPoolConfig, "127.0.0.1", Integer.parseInt(redisPort));
+    }
+
     public String getAuthorizedName(String token) {
         JedisPool pool = getRedisPool();
         Jedis jedis = pool.getResource();
+
+        /*token-email存储到db0中*/
+        jedis.select(0);
+
         String username = jedis.get(token);
         if (username == null)
             return null;
@@ -39,10 +50,68 @@ public class RedisProvider {
     public void setAuthorizeToken(String authorizeToken, String email) {
         JedisPool pool = getRedisPool();
         Jedis jedis = pool.getResource();
+
+        /*token-email存储到db0中*/
+        jedis.select(0);
+
         System.out.println(authorizeToken);
         jedis.set(authorizeToken, email);
         jedis.expire(authorizeToken, Integer.parseInt(redisExpire));
         jedis.close();
         pool.close();
     }
+
+    public void setBannedUser(String author, int banningDays) {
+
+        JedisPool pool = getRedisPool();
+//      JedisPool pool = getLocalRedisPool();   //本地测试用
+
+        Jedis jedis = pool.getResource();
+
+        /*author-banningDays存储到db1中*/
+        jedis.select(1);
+
+        long banningSeconds = banningDays*24*3600;    //禁言时长（秒数）
+        long epoch = System.currentTimeMillis()/1000; //当前unix时间戳
+        long freeTimeStamp = epoch + banningSeconds;
+
+        jedis.set(author,Integer.toString(banningDays));
+        jedis.expireAt(author,freeTimeStamp);
+
+        jedis.close();
+        pool.close();
+    }
+
+
+    public void freeBannedUser(String author){
+
+        JedisPool pool = getRedisPool();
+//      JedisPool pool = getLocalRedisPool();   //本地测试用
+
+        Jedis jedis = pool.getResource();
+
+        /*author-banningDays存储到db1中*/
+        jedis.select(1);
+
+        jedis.del(author); //不存在的key会被忽略
+
+        jedis.close();
+        pool.close();
+
+    }
+
+    public boolean checkIfBanned(String author){
+        JedisPool pool = getRedisPool();
+//      JedisPool pool = getLocalRedisPool();   //本地测试用
+
+        Jedis jedis = pool.getResource();
+
+        /*author-banningDays存储在db1中*/
+        jedis.select(1);
+
+        return jedis.exists(author);
+    }
+
+
+
 }
